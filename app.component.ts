332a3c76fd0a97c32a2614777afdb775347415a1
miccou <30512204+miccou@users.noreply.github.com>
2023-12-21 13:56:12 +1100

    upgrade solution to angular17

--- app.component.ts
+++ app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   standalone: true,
   imports: [CommonModule],
   templateUrl: "app.component.html",
-  styles: [],
 })
 export class AppComponent {
-  title = "stackblitz-angular-tailwind-minimal";
+  title = "angular-tailwind-minimal";
 }
